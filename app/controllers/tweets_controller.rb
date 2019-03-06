1fbf4569d2822ae1b8b764fb47c85ee40773b4ad
kosukekimura <kimkim5364@gmail.com>
2019-03-06 06:11:57 +0000

    new,index,editアクション作成。validation設定完了

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,27 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.all
   end
   
   def new
+    @tweet = Tweet.new
+  end
+  
+  def create
+    @tweet = Tweet.new(params.require(:tweet).permit(:tweet))
+    if @tweet.save
+      redirect_to new_tweet_path,notice: "complete"
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+  
+  def update
+    
   end
   
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,3 @@
 class Tweet < ApplicationRecord
+  validates :tweet, length:{ minimum:10,maximum:140 }
 end
